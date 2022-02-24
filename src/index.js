8e75a01b6cec0d60a972555325eb9c4ca62b644d
Tatiana Rumyantseva <tinarum@MBP-Tatiana.Dlink>
2022-02-25 00:14:09 +0300

    корзина

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './scss/app.scss';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./scss/app.scss";
+import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
